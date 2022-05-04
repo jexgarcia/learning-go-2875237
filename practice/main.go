e5f69b127ab4c5ce6688d63e0f226d21083c98dd
Jexus <jexgarcia@gmail.com>
2022-05-04 17:37:39 +0200

    Commit

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+<<<<<<< Updated upstream
+=======
+	"os"
+	"strconv"
+	"strings"
+>>>>>>> Stashed changes
 )
 
 func main() {
 
 	fmt.Println("Math")
 
+	fmt.Print("Enter num: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Println("the error is", err)
+	} else {
+		fmt.Println("Value of number:", aFloat)
+	}
+	fmt.Println("You entered:", aFloat)
 }
